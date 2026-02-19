6b7ee6f23beb5ca8eb0488efb41e55198d94187b
jevgbur <zeka_ka@inbox.lv>
2026-02-19 12:49:43 +0100

    minor changes in error handling in input validator file

--- input_validators/validate.py
+++ input_validators/validate.py
@@ -16,17 +16,17 @@ except ValueError:
 line = sys.stdin.readline()
 
 if not re.match(r"^(0|[1-9][0-9]*)( (0|[1-9][0-9]*))*$", line):
-    print("Syntax error")
+    print("Invalid input format", file=sys.stderr)
     sys.exit(43)
 
 nums = line.split()
 
 if len(nums) != x:
-     print("Number of elements does not match n-1")
+     print(f"Expected {x} numbers, got {len(nums)}", file=sys.stderr)
      sys.exit(43)
 
 if len(set(nums)) != len(nums):
-     print("Duplicate elements found")
+     print("Duplicate elements found", file=sys.stderr)
      sys.exit(43)
     
 if sys.stdin.read().strip() != "":
